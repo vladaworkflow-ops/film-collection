30d12b7e12507db9423e8b18883ad27445fb11c6
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-05-04 21:14:11 +0200

    feat: configure app routing with main and details routes

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {Home} from './features/pages/home/home';
+import {Detail} from './features/pages/detail/detail';
+import { NotFound } from './features/pages/error-page/error-page';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: "Home Page",
+    component: Home,
+  },
+  {
+    path: 'detail',
+    title: "Detail Page",
+    component: Detail,
+  },
+  {
+    path: '**',
+    component: NotFound
+  }
+];
