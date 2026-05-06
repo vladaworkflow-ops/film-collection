c82dd765c7428972c3a1ca0d2f9d2f70908b6bd8
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-05-06 12:36:12 +0200

    refactor: change cards rendering logic

--- my-app/src/app/features/components/film-cards/film-card.ts
+++ my-app/src/app/features/components/film-cards/film-card.ts
@@ -1,95 +1,45 @@
-import {Component,input, model, ChangeDetectionStrategy} from '@angular/core';
+import {Component,input, model, ChangeDetectionStrategy, inject} from '@angular/core';
+import { Film } from '../../../service/data/films';
+import { FilmService } from '../../../services/film-service';
 
 @Component({
-  selector: 'product-card',
+  selector: 'app-film-card',
   standalone: true,
-  styles: `
-      .card {
-      background: #fff;
-      border-radius: 12px;
-      padding: 12px;
-      box-shadow: 0 4px 12px rgba(0,0,0,0.08);
-      transition: transform 0.2s ease, box-shadow 0.2s ease;
-      }
-
-      .card:hover {
-        transform: translateY(-4px);
-        box-shadow: 0 8px 20px rgba(0,0,0,0.12);
-      }
-
-      img {
-      width: 100%;
-      height: 260px;
-      object-fit: cover;
-      border-radius: 8px;
-      margin-bottom: 10px;
-    }
-
-    h2 {
-      font-size: 16px;
-      margin: 6px 0;
-    }
-
-    p {
-      margin: 2px 0;
-      font-size: 14px;
-      color: #555;
-    }
-
-    .favorite {
-      display: inline-block;
-      cursor: pointer;
-    }
-    .favorite input {
-      display: none;
-    }
-    .favorite .heart::before {
-      content: "♡";
-      font-size: 20px;
-      color: #aaa;
-      transition: 0.2s;
-    }
-
-    .favorite input:checked + .heart::before {
-      content: "❤️";
-      color: red;
-    }
-
-    .favorite:hover .heart::before {
-      transform: scale(1.1);
-    }
-  `,
+  styleUrl: 'cards.css',
   template: `
+  @if(film(); as film){
     <div class="card">
-      <img [src]="posterUrl()" />
-      <h2>{{title() }}</h2>
-      <p>{{year() }} / {{genre() }}</p>
-      <p>⭐{{rating() }}</p>
+      <img [src]="film.posterUrl" />
+      <h2>{{film.title }}</h2>
+      <p>{{film.year }} / {{film.genre }}</p>
+      <p>⭐{{film.rating }}</p>
       <div class="meta">
-        <label class="favorite">
+        <label [class.favorite]="!film.isFavorite">
           <input
             type="checkbox"
             [checked]="checked()"
-            (change)="toggle()"
+            (change)="toggle(film.id)"
           />
           <span class="heart"></span>
         </label>
      </div>
     </div>
+  }
+
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class ProductCard {
-  posterUrl = input.required<string>();
-  title = input.required<string>();
-  year = input.required<number>();
-  genre = input.required<string>();
-  rating = input.required<number>();
-  isFavorite = input<boolean>(false);
-  checked = model.required<boolean>();
+export class FilmCard {
 
-  toggle() {
-  this.checked.set(!this.checked());
+  film = input.required<Film>();
+
+  filmService = inject(FilmService)
+  toggle(id: number) {
+    this.filmService.toggleFavorite(id)
   }
+
+  checked() {
+  }
+
 }
