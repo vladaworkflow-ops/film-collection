ad92d1579ec3c639ebc75d4089008264d7fa8d81
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-05-06 12:30:16 +0200

    refactor: add changes in code

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,22 +1,45 @@
 import { Routes } from '@angular/router';
 import {Home} from './features/pages/home/home';
 import {Detail} from './features/pages/detail/detail';
+import {Favorite} from './features/pages/favorite/favorite';
 import { NotFound } from './features/pages/error-page/error-page';
 
 
 export const routes: Routes = [
   {
     path: '',
-    title: "Home Page",
     component: Home,
   },
   {
     path: 'detail',
-    title: "Detail Page",
     component: Detail,
+  },
+  {
+    path: 'favorite',
+    component: Favorite,
   },
   {
     path: '**',
     component: NotFound
   }
 ];
+
+// export const routes: Routes = [
+//   { path: '', pathMatch: 'full', component: Home },
+//   ...filmDetailsRoutes,
+//   ...aboutRoutes,
+//   ...favoritesRoutes,
+//   ...notFoundRoutes,
+//   { path: '**', redirectTo: '404'},
+// ];
+
+// export const favoritesRoutes: Routes = [
+//   {
+//     path: "favorites",
+//     loadComponent: async () => {
+//       const m = await import("../pages/favorites/favorites");
+//       return m.Favorites;
+//     },
+//   },
+// ];
+
